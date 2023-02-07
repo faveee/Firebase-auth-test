32c77de2bb685bf79776aba169d73a87e7008a98
faveee <abatangift11@gmail.com>
2023-02-07 17:23:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import { useState } from 'react';
 import './App.css';
-import { signInWithGoogle } from "./Firebase";
+import { signInWithGoogle } from "./firebase";
 
 function App() {
+
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const register = async () => {
+
+  }
+
+  const login = async () => {
+
+  }
+  const logout = async () => {
+
+  }
   return (
     <div className="App">
-<button onClick={signInWithGoogle}>hll</button>
+      <div>
+        <h2>Regiser User</h2>
+        <input placeholder='email' Email />
+        <input placeholder='password' Password />
+        <button>Create user</button>
+      </div>
+      <div>
+        <h2>Login</h2>
+        <input placeholder='email' Email />
+        <input placeholder='password' Password />
+        <button>login</button>
+      </div>
+      <div>
+<button onClick={signInWithGoogle}>Sign In with Google</button>
 <h1>{localStorage.getItem("name")}</h1>
+</div>
 </div>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
-import { initializeApp } from "firebase";
-import { getAnalytics } from "firebase";
+import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -14,7 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
 
 const provider = new GoogleAuthProvider()
 
